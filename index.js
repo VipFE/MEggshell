0961ba6ec38502a497c2867355e9d881a9189447
sin01.hu <sin01.hu@vipshop.com>
2016-10-21 16:15:25 +0800

    no message

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /**
  * Created by hoosin on 2016/10/21.
  */
-
 ;(function(root, factory) {
     "use strict";
     if (typeof define === 'function' && define.amd) {
@@ -9,32 +8,32 @@
     } else if (typeof exports === 'object') {
         module.exports = factory();
     } else {
-        root.Meggshell = factory();
+        root.eggshell = factory();
     }
 })(this, function() {
 
-    var Meggshell = {};
+    var eggshell = {};
 
     // 默认4个手指按住屏幕，强制刷新
-    var finger = (function(){
-        var fingerDebugIsOpen =  true;
+    var finger = (function() {
+        var fingerDebugIsOpen = true;
         var fingersDefaultNumber = 4;
 
-        function fn(ev){
-            if( fingerDebugIsOpen === false ){
+        function fn(ev) {
+            if (fingerDebugIsOpen === false) {
                 return;
             }
 
-            if(ev.touches.length === fingersDefaultNumber){
+            if (ev.touches.length === fingersDefaultNumber) {
                 window.location.reload(true);
             }
         }
 
         return {
-            open: function(fingers){
+            open: function(fingers) {
                 fingerDebugIsOpen = true;
 
-                if(typeof fingers === "number" && fingers > 0){
+                if (typeof fingers === "number" && fingers > 0) {
                     fingersDefaultNumber = fingers;
                 }
 
@@ -43,7 +42,7 @@
                 document.addEventListener('touchstart', fn, false);
             },
 
-            close : function(){
+            close: function() {
                 fingerDebugIsOpen = false;
                 document.removeEventListener('touchstart', fn);
             }
@@ -53,7 +52,7 @@
     var errorSign = '__js_error__';
 
     // 打印日志管理
-    var log = (function(){
+    var log = (function() {
 
         // 调试模式是否开启
         var openKey = false;
@@ -84,27 +83,27 @@
 
         // 在chrome上的测试时需要的参数
         // 线上需要注释掉的
-//        var maxJishu = 5;
-//        var fingers = 1;
+        //        var maxJishu = 5;
+        //        var fingers = 1;
 
         var STATUS = {
-            hide : 0,
-            show : 1
+            hide: 0,
+            show: 1
         };
 
         var curStatus = STATUS.hide;
 
-        var cacheName = '_xl_Meggshell_rewriteConsole_key';
+        var cacheName = '_xl_eggshell_rewriteConsole_key';
 
-        function copyConsole(objName, callback){
+        function copyConsole(objName, callback) {
             window[objName] = {};
 
-            for(var i in oldConsole) {
-                if ( typeof oldConsole[i] == 'function' ) {
-                    window[objName][i] = (function(cfn){
-                        return function(){
+            for (var i in oldConsole) {
+                if (typeof oldConsole[i] == 'function') {
+                    window[objName][i] = (function(cfn) {
+                        return function() {
                             var data = Array.prototype.slice.call(arguments);
-                            if(typeof callback == 'function'){
+                            if (typeof callback == 'function') {
                                 callback(data);
                             }
                             //consoleData.push(data);
@@ -120,20 +119,20 @@
 
         // 重写系统console
         function rewriteConsole() {
-            copyConsole('console', function(data){
+            copyConsole('console', function(data) {
                 consoleData.push(data);
             });
         }
 
         function xlconsole() {
-            copyConsole('xlconsole', function(data){
+            copyConsole('xlconsole', function(data) {
                 consoleData.push(data);
             });
         }
 
         // 里面的fn只做一次
-        function doOnce(fn){
-            if(fn.__done === true){
+        function doOnce(fn) {
+            if (fn.__done === true) {
                 return;
             }
 
@@ -144,36 +143,36 @@
         return {
 
             // 默认 只开启 xlconsole 功能
-            // 因为console重写会导致正常的调试的代码位置全部定向Meggshell.js的console，这就不方便了。
-            init: function(){
+            // 因为console重写会导致正常的调试的代码位置全部定向eggshell.js的console，这就不方便了。
+            init: function() {
                 var that = this;
 
                 that.open();
 
-                if( rewriteConsoleKey ){
+                if (rewriteConsoleKey) {
                     rewriteConsole();
                 }
 
-                doOnce(function(){
+                doOnce(function() {
                     // 默认开启 xlconsole
                     xlconsole();
 
-                    document.querySelector('html').addEventListener('touchstart', function(ev){
-                        if( that.isClose() ){
+                    document.querySelector('html').addEventListener('touchstart', function(ev) {
+                        if (that.isClose()) {
                             return;
                         }
 
                         // 如果已显示，3个手指点1次，关闭log窗口
                         if (curStatus === STATUS.show) {
-                            if(ev.touches.length === 3){
+                            if (ev.touches.length === 3) {
                                 that.hide();
                             }
 
                             return;
                         };
 
-                        if(ev.touches.length === fingers){
-                            if( beiginTime === 0 ) {
+                        if (ev.touches.length === fingers) {
+                            if (beiginTime === 0) {
                                 beiginTime = +new Date();
                             }
 
@@ -181,11 +180,11 @@
                             // console.log(jishu);
                         }
 
-                        if( jishu >= maxJishu ) {
+                        if (jishu >= maxJishu) {
                             endTime = +new Date();
                             // 在maxTime触发了彩蛋，显示log
                             // console.log(endTime - beiginTime, maxTime);
-                            if( endTime - beiginTime <= maxTime ) {
+                            if (endTime - beiginTime <= maxTime) {
                                 that.show();
                             }
 
@@ -197,74 +196,74 @@
             },
 
             // 返回收集的log数据
-            get: function(){
+            get: function() {
                 return consoleData;
             },
 
             // 插入样式
-            insertStyle: function(){
-                doOnce(function(){
+            insertStyle: function() {
+                doOnce(function() {
                     var styleStr =
-                        'body .__xl_debug_console_pop, body .__xl_debug_console_pop *{'+
-                        'margin: 0;'+
-                        'font-family: "menlo", "monospace", "Consolas", "Helvetica Neue", Helvetica, STHeiTi, sans-serif;'+
-                        'padding: 0;'+
-                        'font-size:12px !important;'+
-                        'word-break:break-all !important;'+
-                        'word-wrap:break-word !important;'+
-                        '-webkit-user-select: all !important;'+
-                        'user-select: all !important'+
-                        'list-style: none !important;'+
-                        '}'+
-                        'body .__xl_debug_console_pop{'+
-                        'position: fixed;'+
-                        'z-index: 999999;'+
-                        'width: 100%;'+
-                        'height: 100%;'+
-                        'left: 0;'+
-                        'top: 0;'+
-                        'right: 0;'+
-                        'bottom: 0;'+
-                        'background-color: #000;'+
-                        'color: #fff; overflow-y:scroll;'+
-                        'font-size: 0.8em;'+
-                        '}'+
-                        'body .__xl_debug_console_pop ul{'+
-                        'padding: 0 0 40px;'+
-                        '}'+
-                        'body .__xl_debug_console_pop ul li{'+
-                        '    padding: 6px;'+
-                        '}'+
-                        'body .__xl_debug_console_pop ul li b{'+
-                        '    padding:0 4px 0 0;'+
-                        '}'+
-                        'body .__xl_debug_console_pop ul li.error{'+
-                        '   background-color: #E60808; '+
-                        '}'+
-                        'body .__xl_debug_console_pop ul li:nth-child(2n) {'+
-                        '    background-color: #3E3636;'+
-                        '}'+
-                        'body .__xl_debug_console_pop ul li.error:nth-child(2n) {'+
-                        '    background-color: #BB2727;'+
-                        '}'+
-                        'body .__xl_debug_console_pop ul li span{'+
-                        '    padding: 2px 4px !important;'+
-                        '    background-color: #fff;'+
-                        '    color: #000;'+
-                        '    display: inline-block;'+
-                        '    margin: 2px;'+
-                        '    border-radius: 2px;'+
-                        '}'+
-                        'body .__xl_debug_console_pop_close{'+
-                        '    position: fixed;'+
-                        '    right: 0;'+
-                        '    bottom: 0;'+
-                        '    background-color: #b28772;'+
-                        '    width: 40px;'+
-                        '    height: 40px;'+
-                        '    line-height: 40px;'+
-                        '    color: #fff; display:none;'+
-                        '    text-align: center;'+
+                        'body .__xl_debug_console_pop, body .__xl_debug_console_pop *{' +
+                        'margin: 0;' +
+                        'font-family: "menlo", "monospace", "Consolas", "Helvetica Neue", Helvetica, STHeiTi, sans-serif;' +
+                        'padding: 0;' +
+                        'font-size:12px !important;' +
+                        'word-break:break-all !important;' +
+                        'word-wrap:break-word !important;' +
+                        '-webkit-user-select: all !important;' +
+                        'user-select: all !important' +
+                        'list-style: none !important;' +
+                        '}' +
+                        'body .__xl_debug_console_pop{' +
+                        'position: fixed;' +
+                        'z-index: 999999;' +
+                        'width: 100%;' +
+                        'height: 100%;' +
+                        'left: 0;' +
+                        'top: 0;' +
+                        'right: 0;' +
+                        'bottom: 0;' +
+                        'background-color: #000;' +
+                        'color: #fff; overflow-y:scroll;' +
+                        'font-size: 0.8em;' +
+                        '}' +
+                        'body .__xl_debug_console_pop ul{' +
+                        'padding: 0 0 40px;' +
+                        '}' +
+                        'body .__xl_debug_console_pop ul li{' +
+                        '    padding: 6px;' +
+                        '}' +
+                        'body .__xl_debug_console_pop ul li b{' +
+                        '    padding:0 4px 0 0;' +
+                        '}' +
+                        'body .__xl_debug_console_pop ul li.error{' +
+                        '   background-color: #E60808; ' +
+                        '}' +
+                        'body .__xl_debug_console_pop ul li:nth-child(2n) {' +
+                        '    background-color: #3E3636;' +
+                        '}' +
+                        'body .__xl_debug_console_pop ul li.error:nth-child(2n) {' +
+                        '    background-color: #BB2727;' +
+                        '}' +
+                        'body .__xl_debug_console_pop ul li span{' +
+                        '    padding: 2px 4px !important;' +
+                        '    background-color: #fff;' +
+                        '    color: #000;' +
+                        '    display: inline-block;' +
+                        '    margin: 2px;' +
+                        '    border-radius: 2px;' +
+                        '}' +
+                        'body .__xl_debug_console_pop_close{' +
+                        '    position: fixed;' +
+                        '    right: 0;' +
+                        '    bottom: 0;' +
+                        '    background-color: #b28772;' +
+                        '    width: 40px;' +
+                        '    height: 40px;' +
+                        '    line-height: 40px;' +
+                        '    color: #fff; display:none;' +
+                        '    text-align: center;' +
                         '}';
 
                     var head = document.head || document.body;
@@ -277,18 +276,18 @@
             },
 
             // 显示log数据
-            show: function(){
+            show: function() {
                 var that = this;
 
-                if( curStatus === STATUS.show ) return;
+                if (curStatus === STATUS.show) return;
                 curStatus = STATUS.show;
 
                 that.insertStyle();
 
                 var htmlStr =
                     '<p style="padding: 10px 0 0 0; font-size: 16px !important;">【提示：3个手指点1次关闭调试】</p>' +
-                    '<span class="'+popCloseClassName+'" id="'+popCloseId+'">close</span>'+
-                    '<ul id="'+ popUlId +'"></ul>';
+                    '<span class="' + popCloseClassName + '" id="' + popCloseId + '">close</span>' +
+                    '<ul id="' + popUlId + '"></ul>';
 
                 var pop = document.createElement('div');
                 pop.id = popId;
@@ -300,37 +299,37 @@
                 // 插入数据
                 var liStr = '';
 
-                for(var i = 0, len = consoleData.length; i<len; i++) {
+                for (var i = 0, len = consoleData.length; i < len; i++) {
                     var d = consoleData[i];
                     var spanStr = '';
 
                     // 错误信息
-                    if( d[0] === errorSign ) {
+                    if (d[0] === errorSign) {
 
-                        spanStr = '<span><b>[error-message]</b>'+ d[1] +'</span>';
-                        spanStr += '<span><b>[error-source]</b>'+ d[2] +'</span>';
-                        spanStr += '<span><b>[error-line]</b>'+ d[3] +'</span>';
+                        spanStr = '<span><b>[error-message]</b>' + d[1] + '</span>';
+                        spanStr += '<span><b>[error-source]</b>' + d[2] + '</span>';
+                        spanStr += '<span><b>[error-line]</b>' + d[3] + '</span>';
 
-                        liStr += '<li class="error">'+ spanStr +'</li>';
+                        liStr += '<li class="error">' + spanStr + '</li>';
 
-                    }else{  // 正常信息
+                    } else { // 正常信息
 
-                        for(var j = 0; j< d.length; j++){
+                        for (var j = 0; j < d.length; j++) {
                             var s = '';
                             var t = d[j];
 
-                            if( typeof t == 'string' ){
+                            if (typeof t == 'string') {
                                 s = t;
-                            }else if( typeof t == 'function' ) {
+                            } else if (typeof t == 'function') {
                                 s = t.toString();
-                            }else {
+                            } else {
                                 s = JSON.stringify(t);
                             }
 
-                            spanStr += '<span><b>['+ Object.prototype.toString.call(t).split(" ")[1] +'</b>' + s +'</span>';
+                            spanStr += '<span><b>[' + Object.prototype.toString.call(t).split(" ")[1] + '</b>' + s + '</span>';
                         }
 
-                        liStr += '<li>'+ spanStr +'</li>';
+                        liStr += '<li>' + spanStr + '</li>';
                     }
                 }
 
@@ -339,7 +338,7 @@
                 // 绑定关闭按钮
                 var popCloseBtn = document.querySelector("#" + popCloseId);
 
-                function bindPopClose(){
+                function bindPopClose() {
                     that.hide();
                 }
 
@@ -348,10 +347,10 @@
             },
 
             // 隐藏log数据
-            hide: function(){
+            hide: function() {
                 var pop = document.getElementById(popId);
 
-                if(pop) {
+                if (pop) {
                     curStatus = STATUS.hide;
                     pop.parentNode.removeChild(pop);
 
@@ -360,26 +359,26 @@
             },
 
             // 关闭log模式
-            close: function(){
+            close: function() {
                 openKey = false;
             },
 
             // 开启log模式
-            open: function(){
+            open: function() {
                 openKey = true;
             },
 
-            isOpen: function(){
+            isOpen: function() {
                 return openKey;
             },
 
-            isClose: function(){
+            isClose: function() {
                 return !openKey;
             },
 
             // 开启系统 console, xlconsole 的内容也会显示
             // 注意，开启过程中，就无法关闭了
-            openSysConsole: function(){
+            openSysConsole: function() {
                 rewriteConsoleKey = true;
                 this.init();
             }
@@ -392,13 +391,13 @@
     // 默认开启 xlconsole 调试
     log.init();
 
-    Meggshell.finger = finger;
-    Meggshell.log = log;
+    eggshell.finger = finger;
+    eggshell.log = log;
 
-    window.onerror = function(message, source, lineno, colno, error){
+    window.onerror = function(message, source, lineno, colno, error) {
         xlconsole.log(errorSign, message, source, lineno);
         return false;
     };
 
-    return Meggshell;
+    return eggshell;
 });
